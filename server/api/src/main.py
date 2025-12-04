1bffa52b3291f19c7f4952525591cb035d0eebd5
Takuto334 <takuto20007@gmail.com>
2025-12-04 17:23:40 +0900

    OpenweatherのAPIから情報を取得して画像に書き込むweather_forecast.pyを追加

--- server/api/src/main.py
+++ server/api/src/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Response
 from src import create_png
+from src import weather_forecast
 import io
+import requests
 
 app = FastAPI() 
 
@@ -15,3 +17,7 @@ def read_message():
 @app.get("/data")
 def read_data():
     return Response(content=create_png.create_calendar_image(), media_type="image/png")
+
+@app.get("/weather")
+def generate_weather():
+    return Response(content=weather_forecast.generate_weatherInfo(), media_type="image/png")
